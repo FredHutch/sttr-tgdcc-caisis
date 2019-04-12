6928641a6071d39fd3300804534810f6f8544839
Amy Paguirigan <amypaguirigan@gmail.com>
2019-04-12 09:51:08 -0700

    adjust R script and process for app

--- transformForOntology.R
+++ transformForOntology.R
@@ -5,8 +5,7 @@ list.files(pattern = ".xlsx")
 
 nark <- read_xlsx(list.files(pattern = ".xlsx"))
 nark <- nark[!is.na(nark$Field),] #Field cannot be empty or the row will be removed.
-
 nark[] <- lapply(nark, gsub, pattern = "✓", replacement = 1, fixed = TRUE)
-#nark <- rename(nark, "Myeloma" = "M.Myeloma", "HeadNeck" = "H&N")
-
+nark <- nark %>% mutate(FormPath = paste(Table, SubForm, FormNotes, sep = "-"))
+nark$FormPath <- gsub("[-NA]*$", "", nark$FormPath)
 write.csv(nark, file = "forOntology/businessUpFront.csv", row.names = F)
