c1c1bd080c49c2cefe7ad9ea7efdd461d6d92b2b
Amy Paguirigan <amypaguirigan@gmail.com>
2019-04-21 19:52:14 -0700

    better column name!

--- transformForOntology.R
+++ transformForOntology.R
@@ -6,6 +6,6 @@ list.files(pattern = ".xlsx")
 nark <- read_xlsx(list.files(pattern = ".xlsx"))
 nark <- nark[!is.na(nark$Field),] #Field cannot be empty or the row will be removed.
 nark[] <- lapply(nark, gsub, pattern = "✓", replacement = 1, fixed = TRUE)
-nark <- nark %>% mutate(FormPath = paste(Table, SubForm, FormNotes, sep = "-"))
-nark$FormPath <- gsub("[-NA]*$", "", nark$FormPath)
+nark <- nark %>% mutate(Source = paste(Table, SubForm, FormNotes, sep = "-"))
+nark$Source <- gsub("[-NA]*$", "", nark$Source)
 write.csv(nark, file = "forOntology/businessUpFront.csv", row.names = F)
